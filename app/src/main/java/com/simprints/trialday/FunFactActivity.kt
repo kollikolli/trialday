07f5fc0a48b5ed636ac9a090a31bdc1e35916ceb
kollikolli <manuel.kollegger@gmail.com>
2018-02-12 17:29:44 +0000

    Add toast after creating

--- app/src/main/java/com/simprints/trialday/FunFactActivity.kt
+++ app/src/main/java/com/simprints/trialday/FunFactActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.simprints.trialday.data.Database
 import com.simprints.trialday.data.Fact
 
@@ -16,7 +17,10 @@ class FunFactActivity : AppCompatActivity() {
         val submitButton: Button = findViewById(R.id.submitButton)
         val factText: EditText = findViewById(R.id.factText)
         val userText: EditText = findViewById(R.id.nameText)
-        submitButton.setOnClickListener { Database.writeFact(Fact(userText.text.toString(), factText.text.toString())) }
+        submitButton.setOnClickListener {
+            Database.writeFact(Fact(userText.text.toString(), factText.text.toString()))
+            Toast.makeText(this,"Wrote data to firebase", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
